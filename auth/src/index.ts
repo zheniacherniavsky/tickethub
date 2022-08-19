b34e9b5b632861edf78096d311011a37eac0bd3d
Yauheni Charniauski <yauheni.charniauski.work@gmail.com>
2022-08-19 13:40:09 +0400

    chore: ingress-nginx setup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,10 @@ import { json } from 'body-parser';
 const app = express();
 app.use(json());
 
+app.get('/api/users/currentuser', (req, res) => {
+  res.send('testing!');
+});
+
 app.listen(3000, () => {
   console.log('Listening on 3000');
 });
