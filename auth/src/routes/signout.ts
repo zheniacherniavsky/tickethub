62aaa592b287c8b62a5db6a83b94efe3bc0ba322
Yauheni Charniauski <yauheni.charniauski.work@gmail.com>
2022-09-06 13:01:09 +0400

    feat: add signout route

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,7 +3,8 @@ import express from 'express';
 const router = express.Router();
 
 router.post('/api/users/signout', (req, res) => {
-  res.send('hi there.');
+  req.session = null;
+  res.send(204);
 });
 
 export { router as signoutRouter };
